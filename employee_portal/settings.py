6f00ba6f81f58855f23a31cf210530c7d2f6ceb7
gangeshwar01 <153724522+gangeshwar01@users.noreply.github.com>
2025-07-08 21:15:54 +0530

    Update settings.py

--- employee_portal/settings.py
+++ employee_portal/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-your-secret-key-here')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','https://employee-verification-72r5.onrender.com').split(',')
 
 
 # Application definition
